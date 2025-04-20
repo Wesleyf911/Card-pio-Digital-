41a02a65028007f5a220a3248267f37f9428fb95
Wesleyf911 <wesleysilva911@gmail.com>
2025-04-20 17:35:13 -0300

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,11 +1,6 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => <h1>Cardápio Digital - American Burger</h1>;
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
